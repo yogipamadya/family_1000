e681716202d3dd5b45bdd1532627d61c2b9e01a2
yogipamadya <31554546+yogipamadya@users.noreply.github.com>
2023-03-14 21:21:17 +0700

    Update family1000.py

--- family1000.py
+++ family1000.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import random2 as random
+import numpy as np
 
 # Set page title
 st.set_page_config(page_title="Family 1000 BIRM")
@@ -13,7 +13,7 @@ text_input = st.text_input("Masukkan Jawabannya")
 # Add a submit button
 if st.button("Check Survey"):
     # Generate a random score between 0-100
-    score = random.randint(0, 100)
+    score = int(np.random.uniform(0, 101))
     # Display the score to the user
     st.write(f"Hasil Survey Untuk '{text_input}' adalah {score}")
 
